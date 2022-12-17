8e58eadbda61b635f10d04ed440e358a21a4bc1c
Dominik Kilian <Dominik.Kilian@nordicsemi.no>
2022-12-17 19:21:55 +0100

    k

--- scripts/keys_zip.py
+++ scripts/keys_zip.py
@@ -5,14 +5,10 @@ import pyzipper
 import subprocess
 from pathlib import Path
 
-print('START')
-
 this_dir = Path(__file__).parent
 parent_dir = this_dir.parent
 
 password = os.environ.get('PASSWORD')
-
-print(f'PASS: ${password}')
 
 if password is None:
     print('Please provide a password for the script in PASSWORD action secret (or environment variable if running locally).')
@@ -20,13 +16,9 @@ if password is None:
 
 client_count = int(sys.argv[1]) if len(sys.argv) > 1 else 1
 
-print(f'PASS: ${client_count}')
-
 subprocess.run([sys.executable, this_dir / 'keys.py'], check=True)
-print(f'DONE')
 for i in range(client_count):
     subprocess.run([sys.executable, this_dir / 'client_key.py'], check=True)
-    print(f'DONE')
 
 with pyzipper.AESZipFile(parent_dir / 'keys.zip', 'w', compression=pyzipper.ZIP_DEFLATED, encryption=pyzipper.WZ_AES) as zf:
     zf.setpassword(bytes(password, 'utf-8'))
@@ -43,15 +35,11 @@ with pyzipper.AESZipFile(parent_dir / 'keys.zip', 'w', compression=pyzipper.ZIP_
                  'ssh_host_rsa_key.pub',
                  'identity.secret'):
         zf.writestr(name, (parent_dir / name).read_text())
-        print(f'ADDED {name}')
     for name in parent_dir.iterdir():
         if (name.name == 'client_key') or (name.name == 'client_key.pub'):
             zf.writestr(name.name, (parent_dir / name).read_text())
-            print(f'ADDED {name}')
         if name.name.startswith('client_key_') and name.suffix == '.pub':
             zf.writestr(name.name, (parent_dir / name).read_text())
-            print(f'ADDED {name}')
         if name.name.startswith('client_key_') and name.suffix == '':
             zf.writestr(name.name, (parent_dir / name).read_text())
-            print(f'ADDED {name}')
 
